a04359021bc52d40d0b672f5d4a54eaa085398c4
goatastronaut0212 <goatastronaut0212@proton.me>
2023-03-24 21:12:12 +0700

    Thêm postgresql database và insert dữ liệu sql script
     Changes to be committed:
    	new file:   database/database.sql
    	new file:   database/images/TrungNguyen_LegendClassic_Hop.png
    	new file:   database/insert_values.sql

---
+++ database/database.sql
@@ -0,0 +1,73 @@
+CREATE DATABASE coffee_website;
+
+\c coffee_website;
+
+CREATE TABLE coffee_category (
+    coffee_category_id VARCHAR(20),
+    coffee_category_name        VARCHAR(100),
+    PRIMARY KEY(coffee_category_id)
+);
+
+CREATE TABLE coffee_info (
+    coffee_id             VARCHAR(20), 
+    coffee_category_id    VARCHAR(50),
+    coffee_name           VARCHAR(50),
+    coffee_image          BYTEA,
+    coffee_price          BIGINT,
+    coffee_ingredient     VARCHAR(100),
+    coffee_characteristic VARCHAR(700),
+    coffee_shelf_life     VARCHAR(100),
+    coffee_mass           VARCHAR(100),
+    coffee_instructions   VARCHAR(700),
+    coffee_detail         VARCHAR(700),
+    PRIMARY KEY(coffee_id),
+    CONSTRAINT fk_coffee_category_for_coffee_info FOREIGN KEY(coffee_category_id)
+        REFERENCES coffee_category(coffee_category_id)
+);
+
+CREATE TABLE storage (
+    coffee_id VARCHAR(20),
+    amount BIGINT CHECK (amount > 0 AND amount <= 1000),
+    CONSTRAINT fk_storage_coffee_id FOREIGN KEY(coffee_id)
+        REFERENCES coffee_info(coffee_id)
+);
+
+CREATE TABLE customer_info (
+    customer_id   VARCHAR(20),
+    customer_name VARCHAR(60),
+    phone_number  VARCHAR(14),
+    email         VARCHAR(80),
+    address       VARCHAR(400),
+    PRIMARY KEY(customer_id)
+);
+
+CREATE TABLE discount (
+    discount_code_id VARCHAR(20),
+    event_name       VARCHAR(200),
+    discount_percent REAL,
+    PRIMARY KEY(discount_code_id)
+);
+
+CREATE TABLE bill_info (
+    bill_id          VARCHAR(20),
+    customer_id      VARCHAR(60),
+    discount_code_id VARCHAR(20),
+    address          VARCHAR(400),
+    payment_time     DATE,
+    PRIMARY KEY(bill_id),
+    CONSTRAINT fk_customer FOREIGN KEY(customer_id)
+        REFERENCES customer_info(customer_id),
+    CONSTRAINT fk_discount FOREIGN KEY(discount_code_id)
+        REFERENCES discount(discount_code_id)
+);
+
+CREATE TABLE account (
+    account_id          VARCHAR(20),
+    account_username    VARCHAR(100),
+    account_password    VARCHAR(100),
+    account_displayname VARCHAR(100),
+    email               VARCHAR(100),
+    PRIMARY KEY(account_id)
+);
+
+\i insert_values.sql
---
+++ database/insert_values.sql
@@ -0,0 +1,12 @@
+/* coffee_category */
+INSERT INTO coffee_category VALUES
+('CPC', 'Cà phê chai'),
+('CPHT', 'Cà phê hòa tan'),
+('CPRX', 'Cà phê rang xay'),
+('CPHM', 'Cà phê hạt mộc'),
+('VNCP', 'Viên nén cà phê');
+
+INSERT INTO coffee_info VALUES
+('LegendClassicH', 'CPHT', 'Trung Nguyên Legend Classic Hộp',
+  pg_read_binary_file('/tmp/TrungNguyen_LegendClassic_Hop.png'),
+  70000, NULL, NULL, NULL, NULL, NULL, E'Cảm hứng từ những Bản giao hưởng lừng danh của Thiên tài Âm nhạc người Đức – Ludwig Van Beethoven và ước muốn phụng sự Người đam mê cà phê trên toàn thế giới những Tách cà phê năng lượng tuyệt hảo, các chuyên gia của Tập đoàn cà phê số 1 – Trung Nguyên Legend đã tiên tác nên Tuyệt phẩm cà phê Legend Classic.\nNhững khi không có thời gian đến quán cà phê, bạn có thể tự pha cho mình ly cà phê Legend Classic để nạp năng lượng sáng tạo cho một ngày làm việc.')
