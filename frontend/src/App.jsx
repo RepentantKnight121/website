3caa78ea8950fa9e2f76b30ed115e06babf3b781
goatastronaut0212 <goatastronaut0212@proton.me>
2023-03-25 18:45:03 +0700

    Thêm code frontend
     Changes to be committed:
    	new file:   frontend/index.html
    	new file:   frontend/package-lock.json
    	new file:   frontend/package.json
    	new file:   frontend/src/App.jsx
    	new file:   frontend/src/Menu.jsx
    	new file:   frontend/src/input.css
    	new file:   frontend/src/main.jsx
    	new file:   frontend/tailwind.config.cjs
    	new file:   frontend/vite.config.js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import Menu from './Menu'
+
+function App() {
+  return (
+    <Menu />
+  )
+}
+
+export default App
---
+++ frontend/src/Menu.jsx
@@ -0,0 +1,11 @@
+export default function Menu() {
+  return (
+    <div className="">
+      <ul className="flex list-none">
+        <li>Trang chủ</li>
+        <li>Cửa hàng</li>
+        <li>Về chúng tôi</li>
+      </ul>
+    </div>
+  )
+}
