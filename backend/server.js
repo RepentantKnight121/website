4497c9563ebbf36acd58e282b80cf59ad37735e4
Khoadz299 <khoa.ngkhoa26@gmail.com>
2023-03-27 20:29:45 +0700

    Thêm file customer.js và cập nhật server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 // JavaScript library
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const coffeeCategoryRouter = require('./coffee_category');
+const coffeeCategoryRouter = require("./coffee_category");
+const CustomerInfoRouter = require("./customer_info");
 
 app.use(express.json()); // req.body
-app.use('/coffee-category', coffeeCategoryRouter);
+app.use("/coffee-category", coffeeCategoryRouter);
+app.use("/customer_info", coffeeCategoryRouter);
 
 app.listen(5678, () => {
-  console.log('server started at port 5678');
+  console.log("server started at port 5678");
 });
