85e2bbbc0fc8bc1097a61e519194dbf787e20d12
lxhoanq <lamxuanhoangjr@gmail.com>
2023-03-28 22:34:01 +0700

    Thêm file: discount.js

--- backend/discount.js
+++ backend/discount.js
@@ -1,66 +1,71 @@
 const express = require('express');
 const router = express.Router();
+
+// Connect to database
 const pool = require('./database/postgresql');
 
-
-// Lấy danh sách tất cả các mã giảm giá
 router.get('/', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM discount');
-    res.json(result.rows);
+    const getAlldiscount = await pool.query(
+      `SELECT * FROM discount;`
+    );
+    res.json(getAlldiscount.rows);
   } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
+    console.error(err.message);
   }
 });
 
-// Lấy thông tin chi tiết của một mã giảm giá
 router.get('/:id', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM discount WHERE discount_code_id=1', [req.params.id]);
-    if (result.rows.length === 0) {
-      return res.status(404).send('Discount code not found');
-    }
-    res.json(result.rows[0]);
+    const getCoffeeCategory = await pool.query(
+      `SELECT * FROM discount WHERE discount_code_id='${req.params.id}';`
+    );
+    res.json(getCoffeeCategory.rows);
   } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
+    console.error(err.message);
   }
 });
 
-// Tạo mới một mã giảm giá
-router.post('/', async (req, res) => {
-  const { discount_code_id, event_name, discount_percent } = req.body;
+router.post('/new', async (req, res) => {
   try {
-    const result = await pool.query('INSERT INTO discount (discount_code_id, event_name, discount_percent) VALUES (1, 2, 3) RETURNING *', [discount_code_id, event_name, discount_percent]);
-    res.status(201).json(result.rows[0]);
+    const { discount_code_id, event_name, discount_percent } = req.body;
+    const newDiscount = await pool.query(
+      `INSERT INTO discount VALUES ('${discount_code_id}', '${event_name}', '${discount_percent}');`
+    );
+    const getDiscount = await pool.query(
+      `SELECT * FROM discount WHERE discount_code_id='${discount_code_id}';`
+    );
+    res.json(getDiscount.rows);
   } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
+    console.error(err.message);
   }
 });
 
-// Cập nhật thông tin của một mã giảm giá
-router.put('/:id', async (req, res) => {
-  const { event_name, discount_percent } = req.body;
+router.put('/change', async (req, res) => {
   try {
-    const result = await pool.query('UPDATE discount SET event_name=1, discount_percent=2 WHERE discount_code_id=3 RETURNING *', [event_name, discount_percent, req.params.id]);
-    if (result.rows.length === 0) {
-      return res.status(404).send('Discount code not found');
-    }
-    res.json(result.rows[0]);
+    const { discount_code_id, event_name, discount_percent } = req.body;
+    const changeDiscount = await pool.query(
+      `UPDATE discount SET event_name='${event_name}, discount_percent='${discount_percent} ' WHERE discount_code_id='${discount_code_id}';`
+    );
+    const getDiscount = await pool.query(
+      `SELECT * FROM discount WHERE discount_code_id='${discount_code_id}';`
+    );
+    res.json(getDiscount.rows);
   } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
+    console.error(err.message);
   }
 });
-// Xóa một mã giảm giá
-router.delete('/:id', (req, res, next) => {
-  const id = req.params.id;
-  pool.query('DELETE FROM discount WHERE discount_code_id = 1', [id], (error, result) => {
-    if (error) {
-      throw error;
-    }
-    res.status(200).send(`Discount deleted with ID: ${id}`);
-  });
-});
+
+router.delete('/remove', async (req, res) => {
+  try {
+    const { discount_code_id } = req.body;
+    const removeDiscount = await pool.query(
+      `DELETE FROM discount WHERE discount_code_id='${discount_code_id}';`
+    );
+    res.send("Delete successfully");
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+module.exports = router;
