186d1dec18f409a15bcf16bbe68daf14dc19d574
hiep200311 <hiepm088@gmail.com>
2023-03-28 14:55:39 +0700

    Thêm input

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ const app = express();
 
 const coffeeCategoryRouter = require("./coffee_category");
 const CustomerInfoRouter = require("./customer_info");
+const accountRouter = require("./account");
 
 app.use(express.json()); // req.body
 app.use("/coffee-category", coffeeCategoryRouter);
 app.use("/customer-info", CustomerInfoRouter);
+app.use("/account", accountRouter);
 
 app.listen(5678, () => {
-  console.log("server started at port 5678");
+  console.log("server started at port");
 });
