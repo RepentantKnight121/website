3a492cebf73e40ff0fab3d19ef5aab2328a96d4d
hiep200311 <hiepm088@gmail.com>
2023-03-28 15:57:38 +0700

    Thêm input billInfo

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,14 @@ const coffeeCategoryRouter = require("./coffee_category");
 const CustomerInfoRouter = require("./customer_info");
 const accountRouter = require("./account");
 const billdetailRouter = require("./bill_detail");
+const billinfoRouter = require("./bill_info");
 
 app.use(express.json()); // req.body
 app.use("/coffee-category", coffeeCategoryRouter);
 app.use("/customer-info", CustomerInfoRouter);
 app.use("/account", accountRouter);
 app.use("/bill_detail", billdetailRouter);
+app.use("/bill_info", billinfoRouter);
 
 app.listen(5678, () => {
   console.log("server started at port");
