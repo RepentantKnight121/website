1da240949ddc81f7782d13d19fd4cc1d0f875280
goatastronaut0212 <goatastronaut0212@proton.me>
2023-03-30 18:45:02 +0700

    Thêm coffee-info frontend
     Changes to be committed:
    	modified:   ../../../App.jsx
    	new file:   ListCoffeeInfo.jsx
    	new file:   ReturnTableListCoffeeInfo.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import Menu from './Menu'
+import React from 'react';
+
+import Menu from './components/Menu';
+//import ListCoffeeCategory from './components/admin/coffee-category/ListCoffeeCategory';
+import ListCoffeeInfo from './components/admin/coffee-info/ListCoffeeInfo';
+import Footer from './components/Footer';
 
 function App() {
   return (
+  <>
     <Menu />
+    <ListCoffeeInfo /> 
+    <Footer />
+  </>
   )
 }
 
-export default App
+export default App;
