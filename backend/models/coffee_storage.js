c324e05d4be2257b1397b5916ed375b14f3a665b
lxhoanq <lamxuanhoangjr@gmail.com>
2023-04-04 18:54:19 +0700

    Thêm code backend coffee_storage

--- backend/models/coffee_storage.js
+++ backend/models/coffee_storage.js
@@ -1,21 +1,18 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database/postgresql');
 
-const CoffeeStorage = sequelize.define('coffee_storage', {
+const Coffe_storage = sequelize.define('coffee_storage', {
   coffee_id: {
     type: DataTypes.STRING(20) ,
-    references: {
-      model: 'coffee_info', 
-      key: 'coffee_id' 
-    }
+    allowNull: true
   },
   coffee_amount: {
     type: DataTypes.BIGINT,
-    allowNull : false 
+    allowNull : true 
   }
   }, {
     tableName: 'coffee_storage',
     timestamps: false // Bỏ qua thuộc tính `created_at` và `updated_at` tự động của Sequelize
 });
 
-module.exports = CoffeeStorage;
+module.exports = Coffe_storage;
