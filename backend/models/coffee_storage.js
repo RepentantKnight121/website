27a7485b109f0b02d87f34cb44f9fe31f2ea7248
lxhoanq <lamxuanhoangjr@gmail.com>
2023-04-05 10:31:22 +0700

    Revert " Thêm code backend coffee_storage"
    This reverts commit c324e05d4be2257b1397b5916ed375b14f3a665b.

--- backend/models/coffee_storage.js
+++ backend/models/coffee_storage.js
@@ -1,18 +1,21 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database/postgresql');
 
-const Coffe_storage = sequelize.define('coffee_storage', {
+const CoffeeStorage = sequelize.define('coffee_storage', {
   coffee_id: {
     type: DataTypes.STRING(20) ,
-    allowNull: true
+    references: {
+      model: 'coffee_info', 
+      key: 'coffee_id' 
+    }
   },
   coffee_amount: {
     type: DataTypes.BIGINT,
-    allowNull : true 
+    allowNull : false 
   }
   }, {
     tableName: 'coffee_storage',
     timestamps: false // Bỏ qua thuộc tính `created_at` và `updated_at` tự động của Sequelize
 });
 
-module.exports = Coffe_storage;
+module.exports = CoffeeStorage;
