f94384b9141bd39ef5cef117b84c0fbef69c13bc
lxhoanq <lamxuanhoangjr@gmail.com>
2023-04-05 10:28:44 +0700

    Revert " Thêm code backend coffee_storage"
    This reverts commit fac752f54a5d233a653fdc104edfc244b487f46b.

--- backend/access/coffee_storage.js
+++ backend/access/coffee_storage.js
@@ -1,106 +1,66 @@
-const Coffe_storage = require('../models/coffee_storage');
+const express = require('express');
+const router = express.Router();
 
-const getCoffe_storageById = (id) => {
-  return new Promise((resolve, reject) => {
-    Coffe_storage.findOne({
-      raw: true,
-      attributes: [
-        'coffee_id',
-        'coffee_amount'
-      ],
-      where: { coffee_id: id }
-    })
-    .then((coffee_storage) => {
-      if (!coffee_storage) {
-        console.warn(`No coffee_storageDeleted found with id ${id}`);
-        resolve(null);
-      } else {
-        console.log(`Coffee_storageDeleted found with id ${id}`);
-        resolve(coffee_storage);
-      }
-    })
-    .catch((error) => {
-      console.error(`Error finding coffee_storageDeleted with id ${id}: ${error.message}`);
-      reject(error);
-    });
-  });
-};
+// Connect to database
+const pool = require('../database/postgresql');
 
-const getAllCoffe_storage = () => {
-  return new Promise((resolve, reject) => {
-    Coffe_storage.findAll({
-      raw: true,
-      attributes: [
-        'coffee_id',
-        'coffee_amount'
-      ]
-    })
-    .then(coffee_storages => {
-      const allCoffee_storages = coffee_storages.map(coffee_storage => {
-        return {
-          coffee_id: coffee_storage.coffee_id,
-          coffee_amount: coffee_storage.coffee_amount
-        }
-      });
-      console.log(allCoffee_storages);
-      resolve(allCoffee_storages);
-    })
-    .catch(err => {
-      console.error(err);
-      reject(err);
-    });
-  });
-};
-
-const createCoffe_storage = async (newCoffee_storage) => {
+router.get('/', async (req, res) => {
   try {
-    const Coffee_storageCreated = await Coffe_storage.create({
-      coffee_id: newCoffee_storage.coffee_id,
-      coffee_amount: newCoffee_storage.coffee_amount
-    });
-    console.log(`Created coffee_storageDeleted with id ${Coffee_storageCreated.coffee_id}`);
-    return Coffee_storageCreated; // return the created account object
-  } catch (error) {
-    console.error(`Error creating coffee_storageDeleted: ${error.message}`);
-    return null;
+    const getAllStorage = await pool.query(`SELECT * FROM coffee_storage;`);
+    res.status(200).json(getAllCustomer.rows);
+  } catch (err) {
+    res.status(400);
   }
-};
+});
 
-const updateCoffe_storage = async (id, updatedCoffee_storage) => {
+router
+  .route('/:id')
+  .get(async (req, res) => {
+    try {
+      const getStorageID = await pool.query(
+        `SELECT * FROM coffee_storage WHERE coffee_id='${req.params.id}';`
+      );
+      res.status(200).json(getUserByID.rows);
+    } catch (err) {
+      res.status(400);
+  }})
+  .put(async (req, res) => {
+    try {
+      const coffee_amount =req.body;
+      const changeCoffeeStorage = await pool.query(
+        `UPDATE coffee_storage SET
+          coffee_amount='${coffee_amount}' WHERE coffee_id='${req.params.id}';`
+      );
+      const getCoffeeStorageByID = await pool.query(
+        `SELECT * FROM coffee_storage WHERE coffee_id='${req.params.id}';`
+      );
+      res.status(200).json(getCoffeeStorageByID.rows);
+    } catch (err) {
+      res.status(400);
+  }})
+  .delete(async (req, res) => {
+    try {
+      const removeCoffeeStorage = await pool.query(
+        `DELETE FROM coffee_storage WHERE coffee_id='${req.params.id}';`
+      );
+      res.status(200).send("Delete successfully");
+    } catch (err) {
+      res.status(400);
+  }})
+
+router.post("/new", async (req, res) => {
   try {
-    const coffee_storageUpdated = await Coffe_storage.update(
-    {
-      coffee_id: updatedCoffee_storage.coffee_id,
-      coffee_amount: updatedCoffee_storage.coffee_amount
-    },
-    {
-      where: { coffee_id: id }
-    });
-    console.log(`Updated coffee_storageDeleted(s) with id ${id}`);
-    return coffee_storageUpdated;
-  } catch (error) {
-    console.error(`Error updating coffee_storageDeleted with id ${id}: ${error.message}`);
-    return null;
+    const { coffee_id, coffee_amount } = req.body;
+    const newCoffeeStorage = await pool.query(
+      `INSERT INTO coffee_storage VALUES ('${coffee_id}', '${coffee_amount}');`
+    );
+    const getNewCoffeeStorage = await pool.query(
+      `SELECT * FROM coffee_storage WHERE coffee_id='${customer_id}';`
+    );
+    res.status(200).json(getNewCoffeeStorage.rows);
+  } catch (err) {
+    res.status(400);
   }
-};
+});
 
-const deleteCoffe_storage = async (id) => {
-  try {
-    const coffee_storageDeleted = await Account.destroy({
-      where: { coffee_id: id }
-    });
-    console.log(`Deleted coffee_storageDeleted(s) with id ${id}`);
-    return coffee_storageDeleted;
-  } catch (error) {
-    console.error(`Error deleting coffee_storageDeleted with id ${id}: ${error.message}`);
-    return false;
-  }
-};
-
-module.exports = {
-  getCoffe_storageById,
-  getAllCoffe_storage,
-  createCoffe_storage,
-  updateCoffe_storage,
-  deleteCoffe_storage
-};
+module.exports = router;
--- backend/access/customer_info.js
+++ backend/access/customer_info.js
@@ -1,121 +1,74 @@
-const Customer_info = require('../models/customer_info');
+const express = require("express");
+const router = express.Router();
 
-const getcustomerinfoById = (id) => {
-  return new Promise((resolve, reject) => {
-    Customer_info.findOne({
-      raw: true,
-      attributes: [
-        'customer_id',
-        'customer_name',
-        'customer_phone_number',
-        'customer_email',
-        'customer_address'
-      ],
-      where: { customer_id: id }
-    })
-    .then((customer_info) => {
-      if (!customer_info) {
-        console.warn(`No customer_info found with id ${id}`);
-        resolve(null);
-      } else {
-        console.log(`customer_info found with id ${id}`);
-        resolve(customer_info);
-      }
-    })
-    .catch((error) => {
-      console.error(`Error finding customer_info with id ${id}: ${error.message}`);
-      reject(error);
-    });
-  });
-};
+// Connect to database
+const pool = require("../database/postgresql");
 
-const getAllcustomer_info = () => {
-  return new Promise((resolve, reject) => {
-    Customer_info.findAll({
-      raw: true,
-      attributes: [
-        'customer_id',
-        'customer_name',
-        'customer_phone_number',
-        'customer_email',
-        'customer_address'
-      ]
-    })
-    .then(Customers_info => {
-      const allCustomers = Customers_info.map(customer_info => {
-        return {
-          customer_id: customer_info.customer_id,
-          customer_name: customer_info.customer_name,
-          customer_phone_number: customer_info.customer_phone_number,
-          customer_email: customer_info.customer_email,
-          customer_address: customer_info.customer_address
-        }
-      });
-      console.log(allCustomers);
-      resolve(allCustomers);
-    })
-    .catch(err => {
-      console.error(err);
-      reject(err);
-    });
-  });
-};
-
-const createCustomer_info = async (newCustomer_info) => {
+router.get("/", async (req, res) => {
   try {
-    const customer_infoCreated = await Customer_info.create({
-          customer_id: newCustomer_info.customer_id,
-          customer_name: newCustomer_info.customer_name,
-          customer_phone_number: newCustomer_info.customer_phone_number,
-          customer_email: newCustomer_info.customer_email,
-          customer_address: newCustomer_info.customer_address
-    });
-    console.log(`Created customer_info with id ${customer_infoCreated.customer_id}`);
-    return customer_infoCreated; // return the created customer_infoCreated object
-  } catch (error) {
-    console.error(`Error creating customer_info: ${error.message}`);
-    return null;
+    const getAllCustomer = await pool.query(`SELECT * FROM customer_info;`);
+    res.status(200).json(getAllCustomer.rows);
+  } catch (err) {
+    res.status(400);
   }
-};
+});
 
-const updatecustomer_info = async (id, updateCustomer_infoData) => {
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    try {
+      const getUserByID = await pool.query(
+        `SELECT * FROM customer_info WHERE customer_id='${req.params.id}';`
+      );
+      res.status(200).json(getUserByID.rows);
+    } catch (err) {
+      res.status(400);
+  }})
+  .put(async (req, res) => {
+    try {
+      const { customer_id, customer_name, phone_number, email, address } =
+        req.body;
+      const changeCustomer_info = await pool.query(
+        `UPDATE customer_info SET
+          customer_name='${customer_name}',
+          phone_number ='${phone_number}',
+          email ='${email}',
+          address ='${address}'
+          WHERE customer_id='${req.params.id}';`
+      );
+      const getCustomerInfoByID = await pool.query(
+        `SELECT * FROM customer_info WHERE customer_id='${req.params.id}';`
+      );
+      res.status(200).json(getCustomerInfoByID.rows);
+    } catch (err) {
+      res.status(400);
+  }})
+  .delete(async (req, res) => {
+    try {
+      const removeUser = await pool.query(
+        `DELETE FROM customer_info WHERE customer_id='${req.params.id}';`
+      );
+      res.status(200).send("Delete successfully");
+    } catch (err) {
+      res.status(400);
+  }})
+
+router.post("/new", async (req, res) => {
   try {
-    const customer_infoUpdated = await Customer_info.update(
-    {
-      customer_id: updateCustomer_infoData.customer_id,
-      customer_name: updateCustomer_infoData.customer_name,
-      customer_phone_number: updateCustomer_infoData.customer_phone_number,
-      customer_email: updateCustomer_infoData.customer_email,
-      customer_address: updateCustomer_infoData.customer_address
-    },
-    {
-      where: { customer_id: id }
-    });
-    console.log(`Updated customer_info with id ${id}`);
-    return customer_infoUpdated;
-  } catch (error) {
-    console.error(`Error updating customer_info with id ${id}: ${error.message}`);
-    return null;
-  }
-};
+    const { customer_id, customer_name, phone_number, email, address } =
+      req.body;
 
-const deleteCustomer_info = async (id) => {
-  try {
-    const customer_infoDeleted = await Customer_info.destroy({
-      where: { customer_id: id }
-    });
-    console.log(`Deleted customer_info with id ${id}`);
-    return customer_infoDeleted;
-  } catch (error) {
-    console.error(`Error deleting customer_info with id ${id}: ${error.message}`);
-    return false;
+    const NewUser = await pool.query(
+      `INSERT INTO customer_info VALUES
+      ('${customer_id}', '${customer_name}', '${phone_number}', '${email}', '${address}');`
+    );
+    const getNewUser = await pool.query(
+      `SELECT * FROM customer_info WHERE customer_id ='${customer_id}';`
+    );
+    res.status(200).json(getNewUser.rows);
+  } catch (err) {
+    res.status(400);
   }
-};
+});
 
-module.exports = {
-  getcustomerinfoById,
-  getAllcustomer_info,
-  createCustomer_info,
-  updatecustomer_info,
-  deleteCustomer_info
-};
+module.exports = router;
