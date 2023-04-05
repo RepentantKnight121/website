6aa22e841032bae0ddd36e7e515b90ee2e7fc17e
Khoadz299 <khoa.ngkhoa26@gmail.com>
2023-04-05 22:41:03 +0700

    add file bill_detail.js ở 2 folder

---
+++ backend/controllers/bill_detail.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const BillDetail = require("../access/bill_detail");
+
+router.get('/', async (req, res) => {
+  try {
+    const getAllBillDetail = await BillDetail.getAll();
+    res.status(200).json(getAllBillDetail);
+  } catch (err) {
+    res.status(400);
+  }
+});
+
+router
+  .route(':/id')
+  .get(async (req, res) => {
+    try {
+      const getBillDetail = await BillDetail.getDetailByID(req.params.id);
+      res.status(200).json(getBillDetail);
+    } catch (err) {
+      res.status(400);
+  }})
+  .put(async (req, res) => {
+    try {
+      await BillDetail.update( idDetail, req.body);
+      res.send("Updated successfully");
+    } catch (err) {
+      res.status(500).json({ error: 'Internal Server Error' });
+  }})
+  .delete(async (req, res) => {
+    try {      
+      await  BillDetail.remove(idDetail);
+      res.status(200).send('Delete successfully');
+    } catch (err) {
+      res.status(500).json({ error: 'Internal Server Error' });
+  }})
+
+router.post('/', async (req, res) => {
+  try {
+    await BillDetail.create(idDetail , req.body);
+    res.status(200).send('Create successfully');;
+  } catch (err) {
+    res.status(400);
+  }
+});
+
+module.exports = router;
