a661f067a1416284f87695dadf854fb6a557645e
Dê bảnh <119769817+goatastronaut0212@users.noreply.github.com>
2023-04-08 13:25:50 +0700

    Login và Register Page (#26)
    * Code frontend
    
    * Code frontend
    
    ---------
    
    Co-authored-by: hiep200311 <hiepm088@gmail.com>

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,17 +1,39 @@
-function Login() {
-  return (
-    <div>
-      <form>
-        <div>
-          <label>Tên đăng nhập: </label>
-          <input type="text"></input>
+
+    function Login() {
+      return (
+        <div class="flex h-screen flex-col bg-gray-100">
+      
+      <div class="mx-2 my-20 grid place-items-center sm:my-auto">
+        <div class="flex">
+          <span class="mx-auto my-20 text-center font-bold"> </span>
         </div>
-        <div>
-          <label>Mật khẩu: </label>
-          <input type="text"></input>
+    
+        
+        <div class="w-11/12 rounded-lg bg-white p-12 px-6 py-10 shadow-md sm:w-8/12 sm:px-10 sm:py-6 md:w-6/12 lg:w-5/12 lg:shadow-lg 2xl:w-4/12">
+          
+          <h2 class="text-center text-3xl font-semibold text-gray-800 lg:text-4xl">Login</h2>
+    
+          <form class="mt-10" method="POST">
+            
+            <label for="email" class="block text-xs font-semibold uppercase text-gray-600">E-mail</label>
+            <input id="email" type="email" name="email" placeholder="e-mail address" autocomplete="email" class="mt-2 block w-full appearance-none border-b-2 border-gray-100 px-1 py-3 text-gray-800 focus:border-gray-200 focus:text-gray-500 focus:outline-none" required />
+    
+            
+            <label for="password" class="mt-2 block text-xs font-semibold uppercase text-gray-600">Password</label>
+            <input id="password" type="password" name="password" placeholder="password" autocomplete="current-password" class="mb-4 mt-2 block w-full appearance-none border-b-2 border-gray-100 px-1 py-3 text-gray-800 focus:border-gray-200 focus:text-gray-500 focus:outline-none" required />
+    
+            
+            <button type="submit" class="mt-10 w-full rounded-sm bg-gray-800 py-3 font-medium uppercase text-white hover:bg-gray-700 hover:shadow-none focus:outline-none">Login</button>
+    
+            
+            <div class="mt-8 text-center text-sm sm:mb-4  sm:flex-wrap">
+              <a href="./register" class="text-center"> Create an Account </a>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
+      
   )
 }
 
